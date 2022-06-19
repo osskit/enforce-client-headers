020cef96ddb0487bc5a783eb218593136a828122
Itamar Kiesler <99740936+ItamarKieslerFabric@users.noreply.github.com>
2022-06-19 11:21:39 +0300

    ESM (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 
-export const requiredHeaders = ['x-api-client', 'x-api-client-version'];
+export const requiredHeaders = ['x-api-client', 'x-api-client-version'] as const;
 
-export default ({ headers = requiredHeaders }: { headers?: string[] } = {}) =>
+export const enforceClientHeaders = ({ headers = requiredHeaders }: { headers?: readonly string[] } = {}) =>
   (req: Request, _: Response, next: NextFunction) => {
     for (const header of headers) {
       if (!req.headers[header]) {
