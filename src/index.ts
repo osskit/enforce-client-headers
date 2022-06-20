d3c4181db9271977af537ea136261a479faa3716
Sharon Grossman <sharong1337@gmail.com>
2022-06-20 10:49:43 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import type { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 
-export const requiredHeaders = ['x-api-client', 'x-api-client-version'] as const;
+export const defaultHeaders = ['x-api-client', 'x-api-client-version'] as const;
 
-export const enforceClientHeaders = ({ headers = requiredHeaders }: { headers?: readonly string[] } = {}) =>
+export const enforceClientHeaders = ({ headers = defaultHeaders }: { headers?: readonly string[] } = {}) =>
   (req: Request, _: Response, next: NextFunction) => {
     for (const header of headers) {
       if (!req.headers[header]) {
